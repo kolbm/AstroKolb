7ee7446c38c26f942067ef5f248f4d6753f698e2
Marc Kolb <kolbm@dunellenschools.org>
2025-03-03 10:03:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import numpy as np
 G = 6.674e-11  # Gravitational constant (m³/kg/s²)
 AU_TO_METERS = 1.496e+11  # Convert AU to meters
 
-# API endpoints
+# API endpoint for solar system objects
 solar_system_api = "https://api.le-systeme-solaire.net/rest/bodies/"
 
 # Dictionary of known celestial bodies with their API identifiers
@@ -41,20 +41,20 @@ celestial_symbols = {
     "Titan": "🜂"
 }
 
-# NASA JPL Image Links for Solar System Objects
-nasa_images = {
-    "Mercury": "https://solarsystem.nasa.gov/system/feature_items/images/18_mercury_new.png",
-    "Venus": "https://solarsystem.nasa.gov/system/feature_items/images/27_venus_jg.png",
-    "Earth": "https://solarsystem.nasa.gov/system/feature_items/images/17_earth.png",
-    "Mars": "https://solarsystem.nasa.gov/system/feature_items/images/19_mars.png",
-    "Jupiter": "https://solarsystem.nasa.gov/system/feature_items/images/16_jupiter_new.png",
-    "Saturn": "https://solarsystem.nasa.gov/system/feature_items/images/28_saturn.png",
-    "Uranus": "https://solarsystem.nasa.gov/system/feature_items/images/29_uranus.png",
-    "Neptune": "https://solarsystem.nasa.gov/system/feature_items/images/30_neptune.png",
-    "Pluto": "https://solarsystem.nasa.gov/system/feature_items/images/31_pluto.png",
-    "Moon": "https://solarsystem.nasa.gov/system/feature_items/images/16_moon.png",
-    "Europa": "https://solarsystem.nasa.gov/system/feature_items/images/14_europa_1600.jpg",
-    "Titan": "https://solarsystem.nasa.gov/system/feature_items/images/20_titan_1600.jpg"
+# Wikipedia GIFs for Solar System Objects
+wikipedia_gifs = {
+    "Mercury": "https://upload.wikimedia.org/wikipedia/commons/d/d0/Mercury.gif",
+    "Venus": "https://upload.wikimedia.org/wikipedia/commons/0/0e/Venus_Rotation_Movie.gif",
+    "Earth": "https://upload.wikimedia.org/wikipedia/commons/3/32/Earth_rotation.gif",
+    "Mars": "https://upload.wikimedia.org/wikipedia/commons/3/34/Spinning_Mars.gif",
+    "Jupiter": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Jupiter_rotation_over_3_hours_with_11_inch_telescope.gif/220px-Jupiter_rotation_over_3_hours_with_11_inch_telescope.gif",
+    "Saturn": "https://upload.wikimedia.org/wikipedia/commons/f/fe/Saturnoppositions-animated.gif",
+    "Uranus": "https://upload.wikimedia.org/wikipedia/commons/2/20/Uranus_orientation_1985-2030.gif",
+    "Neptune": "https://upload.wikimedia.org/wikipedia/commons/6/6d/Neptune.gif",
+    "Pluto": "https://upload.wikimedia.org/wikipedia/commons/f/f9/Pluto_rotation_movie.gif",
+    "Moon": "https://upload.wikimedia.org/wikipedia/commons/c/c0/Lunar_libration_with_phase2.gif",
+    "Europa": "https://upload.wikimedia.org/wikipedia/commons/2/2d/Europa-rotationmovie.gif",
+    "Titan": "https://upload.wikimedia.org/wikipedia/commons/e/e2/PIA02146.gif"
 }
 
 # Function to get planetary data
@@ -115,7 +115,7 @@ if st.button("Fetch Data"):
     symbol = celestial_symbols.get(selected_body, "N/A")
     st.markdown(f"### **Celestial Symbol: {symbol}**")
 
-    # Display the NASA JPL image of the celestial body
-    image_url = nasa_images.get(selected_body, "")
+    # Display the Wikipedia GIF for the celestial body
+    image_url = wikipedia_gifs.get(selected_body, "")
     if image_url:
-        st.image(image_url, caption=f"Image of {selected_body}", use_container_width=True)
+        st.image(image_url, caption=f"Rotating Image of {selected_body}", use_container_width=True)
